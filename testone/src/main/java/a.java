fb00b46932e4fc33524d1ba565a6864a8e3714df
Shuai.Gao <louis_xun@sina.cn>
2020-05-24 15:52:12 +0800

    commit

--- testone/src/main/java/a.java
+++ testone/src/main/java/a.java
@@ -3,7 +3,6 @@ public class a {
     public void c(){
         System.out.println("1111");
         System.out.println("222");
-        System.out.println("3333");
-        System.out.println("3333");
+        System.out.println("1231321313213");
     }
 }
