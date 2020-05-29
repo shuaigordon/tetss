af33efdbd51f20677586b38881061a749c1b2b0a
Shuai.Gao <louis_xun@sina.cn>
2020-05-29 16:30:37 +0800

    12

--- testone/src/main/java/a.java
+++ testone/src/main/java/a.java
@@ -4,7 +4,6 @@ public class a {
         System.out.println("1111");
         System.out.println("222");
         System.out.println("3333");
-
         System.out.println("1231321313213");
     }
 }
